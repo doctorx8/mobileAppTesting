52c5ac5b4d4c63af03a25444a0d3a32fb80e39c8
doctorx8 <xuekrat@hotmail.com>
2021-09-04 23:09:25 +0200

    calculating

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -7,7 +7,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class Calculator {
-    public static void main(String[] args) throws InterruptedException, MalformedURLException {
+    public static void main(String [] args) throws MalformedURLException, InterruptedException {
 
         DesiredCapabilities dc= new DesiredCapabilities();
         dc.setCapability(MobileCapabilityType.AUTOMATION_NAME,"Appium");
@@ -19,6 +19,20 @@ public class Calculator {
         URL url=new URL("http://localhost:4723/wd/hub");
         AndroidDriver<WebElement> driver=new AndroidDriver<WebElement>(url,dc);
         Thread.sleep(5000);
+       WebElement seven= driver.findElementById("com.android.calculator2:id/digit_7");
+        seven.click();
+        WebElement plus=driver.findElementById("com.android.calculator2:id/op_add");
+        plus.click();
+        WebElement three= driver.findElementById("com.android.calculator2:id/digit_3");
+        three.click();
+        WebElement eq=driver.findElementById("com.android.calculator2:id/eq");
+        eq.click();
+        String result=driver.findElementById("com.android.calculator2:id/result").getText();
+        System.out.println(result);
+        Thread.sleep(5000);
+        if(result.equals("10")){
+            System.out.println("correct");
+        }
 
     }
 }
